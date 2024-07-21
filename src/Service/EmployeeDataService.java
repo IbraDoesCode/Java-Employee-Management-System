9884f81e3f583c6a192903fea8f77e5e41f43806
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2024-07-21 15:17:12 +0800

    Refactored class structure

--- src/Service/EmployeeDataService.java
+++ src/Service/EmployeeDataService.java
@@ -15,10 +15,19 @@ public class EmployeeDataService {
 
     private final CsvHandler csvHandler;
     private final String EMPLOYEE_DATA_FILE = "Data/employeeData.csv";
+    private final List<String[]> employeeData;
+
 
 
     public EmployeeDataService() {
         this.csvHandler = new CsvHandler(EMPLOYEE_DATA_FILE);
+
+        try {
+            employeeData = csvHandler.retrieveCsvData(true);
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing employee data", e);
+        }
+
     }
 
     private String[] retrieveFileHeaders() throws IOException, CsvException {
@@ -26,9 +35,7 @@ public class EmployeeDataService {
     }
 
     private boolean recordExists(String[] record) throws IOException, CsvException {
-        List<String[]> data = csvHandler.retrieveCsvData(true);
-
-        for (String[] row : data) {
+        for (String[] row : employeeData) {
             if (Arrays.equals(row, record)) {
                 return true;
             }
@@ -37,30 +44,24 @@ public class EmployeeDataService {
     }
 
     public void addEmployeeRecord(String[] record) throws IOException, CsvException {
-        List<String[]> data = csvHandler.retrieveCsvData(true);
-
         if (recordExists(record)) {
             AlertUtil.showAlert(Alert.AlertType.ERROR,
                     "Employee Record Exists",
                     "An employee record with the same details already exists. Please verify the information and try again.");
+            return;
         }
 
+        employeeData.add(record);
 
-        // Add record to data list
-        data.add(record);
-
-        // write data to the csv file
-        csvHandler.writeDataToCsv(data, retrieveFileHeaders());
+        csvHandler.writeDataToCsv(employeeData, retrieveFileHeaders());
         System.out.println("Record saved");
-
     }
 
-    public List<Employee> retrieveEmployeeData() throws IOException, CsvException {
-        List<String[]> data = csvHandler.retrieveCsvData(true);
-        List<Employee> employeeData = new ArrayList<>();
+    public List<Employee> retrieveListOfEmployeeObject() throws IOException, CsvException {
+        List<Employee> employees = new ArrayList<>();
 
-        for (String[] row : data) {
-            employeeData.add(new Employee(
+        for (String[] row : employeeData) {
+            employees.add(new Employee(
                     Integer.parseInt(row[0]),
                     row[1],
                     row[2],
@@ -84,40 +85,73 @@ public class EmployeeDataService {
                     Double.parseDouble(row[20])
             ));
         }
-        return employeeData;
+        return employees;
     }
 
-    public Employee retrieveEmployeeById(int employeeId) throws IOException, CsvException {
-        for (Employee employee : retrieveEmployeeData()) {
-            if (employee.getEmployeeID() == employeeId) {
-                return employee;
+    public Employee createNewEmployeeFromArrayInput(String[] employeeData) {
+        return new Employee(
+                Integer.parseInt(employeeData[0]),
+                employeeData[1],
+                employeeData[2],
+                employeeData[3],
+                employeeData[4],
+                employeeData[5],
+                employeeData[6],
+                employeeData[7],
+                employeeData[8],
+                employeeData[9],
+                employeeData[10],
+                employeeData[11],
+                employeeData[12],
+                employeeData[13],
+                employeeData[14],
+                Double.parseDouble(employeeData[15]),
+                Double.parseDouble(employeeData[16]),
+                Double.parseDouble(employeeData[17]),
+                Double.parseDouble(employeeData[18]),
+                Double.parseDouble(employeeData[19]),
+                Double.parseDouble(employeeData[20])
+        );
+    }
+
+    public int getNewEmployeeID() {
+        try {
+            int last_id = 34;
+            for (Employee employee : retrieveListOfEmployeeObject()) {
+                if (employee.getEmployeeID() > last_id) {
+                    last_id = employee.getEmployeeID();
+                }
             }
+            return last_id + 1;
+
+        } catch (IOException | CsvException e) {
+            throw new RuntimeException("Error retrieving new employee ID", e);
         }
-        return null;
     }
 
     public void updateEmployeeRecord(String[] record) throws IOException, CsvException {
-        List<String[]> data = csvHandler.retrieveCsvData(true);
 
-        for (int i = 0; i < data.size(); i++) {
-            String[] row = data.get(i);
+        for (int i = 0; i < employeeData.size(); i++) {
+            String[] row = employeeData.get(i);
             if (row[0].equals(record[0])) {
-                data.set(i, record);
+                employeeData.set(i, record);
             }
         }
 
-        csvHandler.writeDataToCsv(data, retrieveFileHeaders());
+        csvHandler.writeDataToCsv(employeeData, retrieveFileHeaders());
     }
 
     public void deleteEmployeeRecord(int employeeId) throws IOException, CsvException {
-        List<String[]> employeesRecord = csvHandler.retrieveCsvData(true);
         List<String[]> updatedEmployeesRecord = new ArrayList<>();
 
-        for (String[] row : employeesRecord) {
+        for (String[] row : employeeData) {
             if (employeeId != Integer.parseInt(row[0])) {
                 updatedEmployeesRecord.add(row);
             }
         }
+
+        employeeData.clear();
+        employeeData.addAll(updatedEmployeesRecord);
 
         csvHandler.writeDataToCsv(updatedEmployeesRecord, retrieveFileHeaders());
         System.out.println("Record deleted");
--- src/Service/UserCredentialService.java
+++ src/Service/UserCredentialService.java
@@ -12,23 +12,29 @@ public class UserCredentialService {
 
     private CsvHandler csvHandler;
     private String userCredentialFile = "Data/userCredentials.csv";
+    List<String[]> userCredentialData;
 
     public UserCredentialService() {
         this.csvHandler = new CsvHandler(userCredentialFile);
+
+        try {
+            userCredentialData = csvHandler.retrieveCsvData(true);
+        } catch (IOException e) {
+            throw new RuntimeException("An error has occurred while initializing user credential data", e);
+        }
     }
 
-    private List<User> retrieveUserCredentials() throws IOException, CsvException {
-        List<String[]> data = csvHandler.retrieveCsvData(true);
+    private List<User> retrieveListOfUserObject() throws IOException, CsvException {
         List<User> userCredentials = new ArrayList<>();
 
-        for (String[] row : data) {
+        for (String[] row : userCredentialData) {
             userCredentials.add(new User(Integer.parseInt(row[0]), row[1], row[2], row[3], row[4]));
         }
         return userCredentials;
     }
 
     public User retrieveUserCredentialByUsername(String username) throws IOException, CsvException {
-        for (User user : retrieveUserCredentials()) {
+        for (User user : retrieveListOfUserObject()) {
             if (user.getUsername().equals(username)) {
                 return user;
             }
