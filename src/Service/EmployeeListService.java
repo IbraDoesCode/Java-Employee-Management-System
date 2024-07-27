7df50b5af2469191385a724b56161d135f74e598
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2024-07-27 23:37:14 +0800

    Added recordExits method to prevent duplicate records

--- src/Service/EmployeeListService.java
+++ src/Service/EmployeeListService.java
@@ -11,11 +11,35 @@ public class EmployeeListService {
         this.employeeList = employeeList;
     }
 
+    private boolean recordExists(Employee newEmp) {
+        for (Employee emp : employeeList) {
+
+            if (emp.getEmployeeID() == newEmp.getEmployeeID() ||
+                emp.getPhoneNumber().equals(newEmp.getPhoneNumber()) ||
+                emp.getAddress().equals(newEmp.getAddress()) ||
+                emp.getTinNumber().equals(newEmp.getTinNumber()) ||
+                emp.getSssNumber().equals(newEmp.getSssNumber()) ||
+                emp.getPhilhealthNumber().equals(newEmp.getPhilhealthNumber()) ||
+                emp.getPagibigNumber().equals(newEmp.getPagibigNumber())) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void addEmployee(Employee employee) {
+        if (recordExists(employee)) {
+            return;
+        }
         employeeList.add(employee);
     }
 
     public void updateEmployee(Employee updatedEmployee) {
+        if (recordExists(updatedEmployee)){
+            return;
+        }
+
         for (int i = 0; i < employeeList.size(); i++) {
             Employee emp = employeeList.get(i);
             if (emp.getEmployeeID() == (updatedEmployee.getEmployeeID())) {
@@ -28,4 +52,5 @@ public class EmployeeListService {
     public void removeEmployee(Employee employee) {
         employeeList.remove(employee);
     }
+
 }
