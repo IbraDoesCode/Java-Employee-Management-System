b9d062ddb52ee8381257955b59b37df9d107f542
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2024-08-03 21:09:24 +0800

    Replace data type of birthday and hire date attributes from string to LocalDate object

--- src/Model/Employee.java
+++ src/Model/Employee.java
@@ -1,15 +1,16 @@
 package Model;
 
+import java.time.LocalDate;
 
 public class Employee {
 
     private final int employeeID;
     private final String lastName;
     private final String firstName;
-    private final String birthday;
+    private final LocalDate birthday;
     private final String address;
     private final String phoneNumber;
-    private final String hireDate;
+    private final LocalDate hireDate;
     private final String sssNumber;
     private final String philhealthNumber;
     private final String tinNumber;
@@ -22,13 +23,12 @@ public class Employee {
     private final double riceSubsidy;
     private final double phoneAllowance;
     private final double clothingAllowance;
-    private final double grossSemiMonthly;
     private final double hourlyRate;
 
-    public Employee(int employeeID, String lastName, String firstName, String birthday, String address,
-    String phoneNumber, String hireDate, String sssNumber, String philhealthNumber, String tinNumber, String pagibigNumber,
+    public Employee(int employeeID, String lastName, String firstName, LocalDate birthday, String address,
+    String phoneNumber, LocalDate hireDate, String sssNumber, String philhealthNumber, String tinNumber, String pagibigNumber,
     String status, String position, String department, String immediateSupervisor, double basicSalary,
-    double riceSubsidy, double phoneAllowance, double clothingAllowance, double grossSemiMonthly, double hourlyRate) {
+    double riceSubsidy, double phoneAllowance, double clothingAllowance, double hourlyRate) {
         this.employeeID = employeeID;
         this.lastName = lastName;
         this.firstName = firstName;
@@ -48,26 +48,25 @@ public class Employee {
         this.riceSubsidy = riceSubsidy;
         this.phoneAllowance = phoneAllowance;
         this.clothingAllowance = clothingAllowance;
-        this.grossSemiMonthly = grossSemiMonthly;
         this.hourlyRate = hourlyRate;
     }
-    
+
     public int getEmployeeID() {
         return employeeID;
     }
-    
+
     public String getLastName() {
         return lastName;
     }
-    
+
     public String getFirstName() {
         return firstName;
     }
-    
-    public String getBirthday() {
+
+    public LocalDate getBirthday() {
         return birthday;
     }
-    
+
     public String getAddress() {
         return address;
     }
@@ -76,14 +75,14 @@ public class Employee {
         return phoneNumber;
     }
 
-    public String getHireDate() {
+    public LocalDate getHireDate() {
         return hireDate;
     }
-    
+
     public String getSssNumber() {
         return sssNumber;
     }
-    
+
     public String getPhilhealthNumber() {
         return philhealthNumber;
     }
@@ -91,23 +90,23 @@ public class Employee {
     public String getTinNumber() {
         return tinNumber;
     }
-    
+
     public String getPagibigNumber() {
         return pagibigNumber;
     }
-    
+
     public String getStatus() {
         return status;
     }
-    
+
     public String getPosition() {
         return position;
     }
-    
+
     public String getDepartment() {
         return department;
     }
-    
+
     public String getImmediateSupervisor() {
         return immediateSupervisor;
     }
@@ -119,21 +118,17 @@ public class Employee {
     public double getRiceSubsidy() {
         return riceSubsidy;
     }
-    
+
     public double getPhoneAllowance() {
         return phoneAllowance;
     }
-    
+
     public double getClothingAllowance() {
         return clothingAllowance;
     }
 
     public double getHourlyRate() {
         return hourlyRate;
-    }
-
-    public double getGrossSemiMonthly() {
-        return grossSemiMonthly;
     }
 
 }
